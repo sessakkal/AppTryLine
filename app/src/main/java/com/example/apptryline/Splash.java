60268226b39a8205d50eb9b57855337132a13e7f
sessakkal <sessakkal@elpuig.xeill.net>
2024-04-23 02:16:47 +0200

    splash?

--- app/src/main/java/com/example/apptryline/Splash.java
+++ app/src/main/java/com/example/apptryline/Splash.java
@@ -1,13 +1,11 @@
 package com.example.apptryline;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,20 +16,30 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        TimerTask task =new TimerTask() {
+
+        // Obtener la referencia de la imagen de la pelota
+        logoSplash = findViewById(R.id.logosplash);
+
+        // Cargar la animación desde el archivo XML
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animacion_pelota);
+
+        // Aplicar la animación a la imagen de la pelota
+        logoSplash.startAnimation(animation);
+
+        // Configurar el temporizador para pasar a la actividad principal después de cierto tiempo
+        TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent intent= new Intent(Splash.this, MainActivity.class);
+                Intent intent = new Intent(Splash.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         };
-        Timer tiempo= new Timer();
-        tiempo.schedule(task,2000);
-
-        //animacion
-        logoSplash = findViewById(R.id.logosplash);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_logo);
-        logoSplash.setAnimation(animation);
+        Timer tiempo = new Timer();
+        tiempo.schedule(task, 1200);
     }
 }
+
+
+
+
