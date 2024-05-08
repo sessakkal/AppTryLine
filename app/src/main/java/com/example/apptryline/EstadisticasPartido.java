d9258eee6741eaf281d3101fb382a16b454d8fae
sessakkal <sessakkal@elpuig.xeill.net>
2024-05-08 14:27:27 +0200

    estadisticas

--- app/src/main/java/com/example/apptryline/EstadisticasPartido.java
+++ app/src/main/java/com/example/apptryline/EstadisticasPartido.java
@@ -1,7 +1,84 @@
 package com.example.apptryline;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import java.util.ArrayList;
+
+public class EstadisticasPartido extends AppCompatActivity {
+
+    private HorizontalBarChart horizontalBarChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.estadisticas_partido);
+
+        horizontalBarChart = findViewById(R.id.tries);
+
+        // Datos de ejemplo para el partido (goles, tiros a puerta, tarjetas, etc.)
+        ArrayList<BarEntry> localData = new ArrayList<>();
+        ArrayList<BarEntry> visitorData = new ArrayList<>();
+
+        // Agregar datos de ejemplo para el equipo local y el equipo visitante
+        localData.add(new BarEntry(1, -7));  // Tres goles para el equipo local
 
 
-public class EstadisticasPartido {
+        visitorData.add(new BarEntry(1, 4));  // Dos goles para el equipo visitante
+
+
+        // Crear conjuntos de datos para el equipo local y el equipo visitante
+        BarDataSet localDataSet = new BarDataSet(localData, "Equipo Local");
+        localDataSet.setColor(Color.GREEN);
+
+        BarDataSet visitorDataSet = new BarDataSet(visitorData, "Equipo Visitante");
+        visitorDataSet.setColor(Color.GREEN);
+
+        // Crear BarData con los conjuntos de datos
+        BarData barData = new BarData(localDataSet, visitorDataSet);
+
+        // Establecer los datos en el gráfico
+        horizontalBarChart.setData(barData);
+        horizontalBarChart.getLegend().setEnabled(false);
+        horizontalBarChart.setDrawBorders(false);
+
+        // Personalizar el aspecto del gráfico según tus preferencias
+        horizontalBarChart.setDrawValueAboveBar(true); // Mostrar los valores encima de las barras
+        horizontalBarChart.getAxisLeft().setAxisMinimum(-10); // Establecer el mínimo del eje izquierdo
+        horizontalBarChart.getAxisRight().setAxisMinimum(-10); // Establecer el mínimo del eje derecho
+
+        // Ocultar la cuadrícula y los números en el eje X
+        XAxis xAxis = horizontalBarChart.getXAxis();
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawLabels(false);
+
+        // Ocultar la cuadrícula y los números en el eje Y izquierdo
+        YAxis leftAxis = horizontalBarChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLabels(false);
+
+        // Ocultar la cuadrícula y los números en el eje Y derecho
+        YAxis rightAxis = horizontalBarChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setDrawLabels(false);
+
+
+        // Desactivar interacción con el gráfic
+        horizontalBarChart.setTouchEnabled(false);
+        horizontalBarChart.setDragEnabled(false);
+        horizontalBarChart.setScaleEnabled(false);
+
+
+
+        // Invalidar el gráfico para que se actualice
+        horizontalBarChart.invalidate();
+    }
 }
+
 
--- app/src/main/java/com/example/apptryline/Partido.java
+++ app/src/main/java/com/example/apptryline/Partido.java
@@ -1,7 +1,9 @@
 package com.example.apptryline;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
@@ -21,7 +23,6 @@ public class Partido extends AppCompatActivity {
     private TextView ubicacionTextoTextView;
     private TextView equipoLocalTextView;
     private TextView equipoVisitanteTextView;
-    private HorizontalBarChart horizontalBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,56 +37,10 @@ public class Partido extends AppCompatActivity {
         ubicacionTextoTextView = findViewById(R.id.ubicacion_texto_partido);
         equipoLocalTextView = findViewById(R.id.equipo_local_partido);
         equipoVisitanteTextView = findViewById(R.id.equipo_visitante_partido);
-        horizontalBarChart = findViewById(R.id.horizontalBarChart);
-
-        // Datos de ejemplo para el partido (goles, tiros a puerta, tarjetas, etc.)
-        ArrayList<BarEntry> localData = new ArrayList<>();
-        ArrayList<BarEntry> visitorData = new ArrayList<>();
-
-        // Agregar datos de ejemplo para el equipo local y el equipo visitante
-        localData.add(new BarEntry(1, -7));  // Tres goles para el equipo local
-        localData.add(new BarEntry(2, -5));  // Cinco tiros a puerta para el equipo local
-
-        visitorData.add(new BarEntry(1, 4));  // Dos goles para el equipo visitante
-        visitorData.add(new BarEntry(2, 9));  // Tres tiros a puerta para el equipo visitante
-
-        // Crear conjuntos de datos para el equipo local y el equipo visitante
-        BarDataSet localDataSet = new BarDataSet(localData, "Equipo Local");
-        localDataSet.setColor(Color.GREEN);
-
-        BarDataSet visitorDataSet = new BarDataSet(visitorData, "Equipo Visitante");
-        visitorDataSet.setColor(Color.GREEN);
-
-        // Crear BarData con los conjuntos de datos
-        BarData barData = new BarData(localDataSet, visitorDataSet);
-
-        // Establecer los datos en el gráfico
-        horizontalBarChart.setData(barData);
-        horizontalBarChart.getLegend().setEnabled(false);
-        horizontalBarChart.setDrawBorders(false);
-
-        // Personalizar el aspecto del gráfico según tus preferencias
-        horizontalBarChart.setDrawValueAboveBar(false); // Mostrar los valores encima de las barras
-        horizontalBarChart.getAxisLeft().setAxisMinimum(-10); // Establecer el mínimo del eje izquierdo
-        horizontalBarChart.getAxisRight().setAxisMinimum(-10); // Establecer el mínimo del eje derecho
-
-        // Ocultar la cuadrícula y los números en el eje X
-        XAxis xAxis = horizontalBarChart.getXAxis();
-        xAxis.setDrawGridLines(false);
-        xAxis.setDrawLabels(false);
-
-        // Ocultar la cuadrícula y los números en el eje Y izquierdo
-        YAxis leftAxis = horizontalBarChart.getAxisLeft();
-        leftAxis.setDrawGridLines(false);
-        leftAxis.setDrawLabels(false);
-
-        // Ocultar la cuadrícula y los números en el eje Y derecho
-        YAxis rightAxis = horizontalBarChart.getAxisRight();
-        rightAxis.setDrawGridLines(false);
-        rightAxis.setDrawLabels(false);
-
-        // Invalidar el gráfico para que se actualice
-        horizontalBarChart.invalidate();
+    }
+    public void irAEstadisticas(View view) {
+        Intent intent = new Intent(this, EstadisticasPartido.class);
+        startActivity(intent);
     }
 }
 
